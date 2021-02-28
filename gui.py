e3c7d801bf7193a166aa699419997528f8854ef6
diddobaggins <joying0614@gmail.com>
2021-02-28 01:24:56 -0800

    update gui

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import threading
 import time
+import random
 
 def game_loop():
     while True:
@@ -8,26 +9,40 @@ def game_loop():
         if frame_stats.winfo_ismapped():
             update_resources()
         if time_tracker.get() == 15:
-            x.set(1)
+            x.set(0)
             word.set("You missed a call while you were at work. 510-029-4391 left a voicemail asking you to call back...")
-
+            while currently_typing.get():
+                print("")
             type_effect("call")
             time_tracker.set(16)
         if time_tracker.get() > 25 and time_tracker.get()%50 == 0 and key_actions["call_0"] == False:
-            while currently_typing:
+            while currently_typing.get():
                 print("")
-            x.set(1)
+            x.set(0)
             word.set("You missed a call while you were at work. 510-029-4391 left a voicemail asking you to call back...")
             type_effect("none")
         if unlocked_dict["food"] == True and resources_dict["food"] == 1:
             resources_dict["food"] = 0
-            while currently_typing:
-                print("")
-            x.set(1)
-            word.set("Your pantry ran out. Soon you will succumb to hunger...")
+            while currently_typing.get():
+                print("currentlytying",currently_typing.get())
+            x.set(0)
+            word.set("Your pantry ran out. Soon you will succumb to hunger if you don't refill...")
             type_effect("n")
         if resources_dict["food"] < -2:
             print("player died")
+        if time_tracker.get() == 90 and key_actions["call_0"] == True:
+            time_tracker.set(91)
+            while currently_typing.get():
+                print("")
+            x.set(0)
+            word.set("Hey. I'm Andy from BuberEats, a startup for food delivery. We want to help deliver you food safely. If you're interested in our service, call me back at 510-291-2222 and you can use our service after a $100 deposit.")
+            type_effect("n")
+        # if random.randint(0, 10000) == 1 and key_actions["call_1"] == False:
+        #     while currently_typing.get():
+        #         print("")
+        #     x.set(0)
+        #     word.set("You missed a call while you were at work. 510-298-2911 left a voicemail asking you to call back...")
+        #     type_effect("n")
 
 def update_resources():
     for key in resources_dict:
@@ -39,7 +54,7 @@ def update_resources():
             if key == "food":
                 food_label.grid(row=1, column=0)
                 if resources_dict["food"] > 8:
-                    food_label.config(text="food: full")
+                    food_label.config(text="food: plenty")
                 elif resources_dict["food"] > 5:
                     food_label.config(text="food: enough")
                 elif resources_dict["food"] > 3:
@@ -56,37 +71,61 @@ def time_increment():
             resources_dict["food"]-=1
 
 def type_effect(place_something):
-    currently_typing = True
+    if x.get() == 0:
+        currently_typing.set(True)
     message.config(text=word.get()[:x.get()],anchor='nw')
     if x.get() < len(word.get()):
         threading.Timer(0.05, type_effect, [place_something]).start()
         x.set(x.get()+1)
         if x.get() == len(word.get()) - 1:
-            currently_typing = False
+            currently_typing.set(False)
             time.sleep(.5)
             if go_work.winfo_ismapped() == False:
                 unlocked_dict["money"] = True
-                go_work.place(x=20, y=130, width=100)
+                go_work.place(x=20, y=130, width=150)
                 frame_stats.place(x=400, y=40)
             if call_number.winfo_ismapped() == False and place_something == "call":
-                call_number.place(x=20, y=160, width=100)
-                call_field.place(x=130, y=160)
+                call_number.place(x=20, y=160, width=150)
+                call_field.place(x=180, y=160)
             if place_something == "call_0":
-                x.set(1)
-                word.set("Hello? God, you finally picked up. It's nuts out there, huh? Nobody knows what to expect anymore. Just look out for yourself and try not to go out too much... You never know when you might get it.")
+                x.set(0)
+                word.set("Hello? God, you finally picked up. It's nuts out there, huh? Nobody knows what to expect anymore. Just look out for yourself and try not to go out too much... You might get it just from a trip to the grocers.")
                 type_effect('done call 0')
                 unlocked_dict["food"] = True
+            if place_something == "call_1":
+                x.set(0)
+                if resources_dict["money"] >= 100:
+                    resources_dict["money"] -= 100
+                    word.set("Hey! Glad to hear that you're interested in us! You won't regret your investment. Our delivery portions are big, and will last you for a bit.")
+                    order_food.place(x=20, y=220, width=150)
+                else:
+                    word.set("sorry dawg we dont fuck with broke bros")
+                type_effect('none')
             if place_something == "done call 0":
-                get_food_button.place(x=20, y=190, width=100)
+                get_food_button.place(x=20, y=190, width=150)
             if place_something == "bad call":
-                x.set(1)
+                x.set(0)
                 word.set("Your call didn't go through. Are you sure you called the right number? Or maybe they're busy...")
                 type_effect('none')
+
+def catch_covid(chance):
+    random_num = random.randint(0, 100)
+    if random_num < chance:
+        while currently_typing.get():
+            True
+        x.set(0)
+        word.set("You test positive for COVID-19. The doctor tells you to stay home, but you know money can save your life.")
+        if resources_dict["money"] > 1000:
+            resources_dict["money"] -= 1000
+        else:
+            resources_dict["money"] = 0
+
 
 def work():
     go_work["state"] = DISABLED
     resources_dict["money"] += 20
     threading.Timer(3, enable_work).start()
+    catch_covid(1)
 
 def enable_work():
     go_work["state"] = NORMAL
@@ -96,7 +135,7 @@ def call():
         input_number = call_field.get()
         call_number["state"] = DISABLED
         threading.Timer(5, enable_call).start()
-        x.set(1)
+        x.set(0)
         word.set("Calling....................")
         input_number_num = ""
         for i in input_number:
@@ -105,13 +144,13 @@ def call():
         if input_number_num == "5100294391":
             key_actions["call_0"] = True
             type_effect("call_0")
-        elif input_number_num == "another num":
-            True
-            # called another call_number
+        elif input_number_num == "5102912222":
+            key_actions["call_1"] = True
+            type_effect("call_1")
         else:
             type_effect("bad call")
     else:
-        x.set(1)
+        x.set(0)
         word.set("You cannot call an empty number...")
         type_effect("none")
 
@@ -119,7 +158,15 @@ def enable_call():
     call_number["state"] = NORMAL
 
 def get_food():
+    catch_covid(5)
     resources_dict["food"] = 10
+    if resources_dict["money"] >= 5:
+        resources_dict["money"] -= 5
+
+def order():
+    resources_dict["food"] = 200
+    if resources_dict["money"] >= 50:
+        resources_dict["money"] -= 50
 
 root = Tk()
 root.title("COVID Adventure Game")
@@ -137,10 +184,11 @@ unlocked_dict = {
 }
 
 key_actions = {
-    "call_0": False
+    "call_0": False,
+    "call_1": False
 }
 
-currently_typing = False
+currently_typing = BooleanVar(root, False)
 time_tracker = IntVar(root, value=0)
 x = IntVar(root, value=1)
 
@@ -153,10 +201,11 @@ thread_loop.start()
 # add frame
 
 # buttons n shit
-go_work = Button(root, text="go to work...",  command=work)
+go_work = Button(root, text="go to work",  command=work)
 call_number = Button(root, text="call number", command=call)
 call_field = Entry(root, width=10)
 get_food_button = Button(root, text="buy food", command=get_food)
+order_food = Button(root, text="order food", command=order)
 
 frame_stats = LabelFrame(root, text="resources...",padx=5,pady=5)
 

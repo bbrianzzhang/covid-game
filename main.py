c3fdcb50a1d2318cddc62728383ead09d0e06594
bbrianzzhang <bbrianzzhang@gmail.com>
2021-02-27 16:50:06 -0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 
 commands = ['go to work', 'end day']
-money = 10000
+money = 100000
 days = 0
-covidChance = 20
+covidChanceGroceries = 20
+covidChanceWork = 5
+foodDelivery = False
+government = True
 
 
 def receive_input():
@@ -15,31 +18,68 @@ def receive_input():
     while True:
         user_input = input("Choose an action")
         if user_input in commands:
-            days += 1
             return user_input
         else:
             print("Not a valid action.")
 
 
-def go_outside():
-    global money
-    x = random.randint(0, 100)
-    if x < covidChance:
-        print("While outside, you caught COVID. You recover, but the price is high.")
-        money -= 5000
+def check_conditions():
+    global money, foodDelivery, days
+    if money > 15000 and foodDelivery == False:
+        answer = input("You are given the opportunity to invest in a food delivery company. Would you like to invest $5000? ("
+              "Y/N)")
+        if answer == "Y":
+            print("Money invested.")
+            money -= 5000
+            foodDelivery = True
+    if days > 50:
+        print("You are encouraged by a co-worker to run for government.")
+        print("It costs $50000 to run.")
+        commands.append("run for government")
+
+
+def run_for_government():
+    global money, government
+    money -= 50000
+    x = random.randint(1, 2)
+    if x==1:
+        print("Your campaign was not successful. You must try again another day.")
+    else:
+        print("Your campaign was successful! Congratulations.")
+        print("You now have more options in your daily life.")
+        government = True
+        commands.append("create mask production facility")
+        commands.append("create COVID testing center")
+        commands.append("create vaccine research center")
+
 
 
 def get_groceries():
     print("You go outside to get groceries.")
     global money
     money -= 200
-    go_outside()
+    x = random.randint(0, 100)
+    if foodDelivery:
+        if input("Would you like to order your groceries instead? It will cost extra money but you have no chance of "
+                 "getting COVID (Y/N)") == "Y":
+            money -= 50
+            print("You ordered groceries online.")
+            return
+        else:
+            print("You chose to go out yourself.")
+
+    if x < covidChanceGroceries:
+        print("While outside, you caught COVID. You recover, but the price is high.")
+        money -= 5000
 
 
 def work():
     global money
     money += 600
-    go_outside()
+    x = random.randint(0, 100)
+    if x < covidChanceWork:
+        print("While outside, you caught COVID. You recover, but the price is high.")
+        money -= 5000
 
 
 while True:
@@ -47,16 +87,22 @@ while True:
     print("Day " + str(days))
     print("You have " + str(money) + " money.")
     canWork = True
+    check_conditions()
     while not dayOver:
         action = receive_input()
         if action == 'end day':
             dayOver = True
+            days += 1
         elif action == 'go to work':
             if canWork:
                 work()
                 canWork = False
             else:
                 print("You already went to work.")
-    days += 1
+        elif action == 'run for government':
+            if money > 50000:
+                run_for_government()
+            else:
+                print("You do not have enough money to run for government.")
     if days % 7 == 0:
         get_groceries()
